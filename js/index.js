50d7b3bd672d968150b80a25c28efc7619bdadd8
Stephanie Cueto <122324572+sxcueto@users.noreply.github.com>
2025-05-15 02:04:57 -0400

    form and functionality added (#7)
    * form and functionality added
    
    * added more things

--- js/index.js
+++ js/index.js
@@ -4,11 +4,13 @@ const container = document.getElementById('myFooter');
 const addFooter = document.createElement('footer');
 container.appendChild(addFooter);
  
+//get current year
 const d = new Date();
  let thisYear = d.getFullYear();
 
  const footer = document.querySelector('footer');
 
+ //copyright
  const copyright = document.createElement('p');
 copyright.innerHTML =`Stephanie Cueto ${thisYear}`;
 
@@ -25,3 +27,45 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+//form section
+
+//callback for submit
+function onFormSubmit(event){
+     event.preventDefault(); 
+
+     const data = new FormData(event.target);
+     console.log(data);
+const userName = data.get("usersName");
+const email = data.get("email");
+const usersMessage = data.get("usersMessage");
+    
+console.log(userName);
+console.log(email);
+console.log(usersMessage);
+
+//displays messages below message section
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.getElementsByTagName("ul");
+    const newMessage = document.createElement("li");
+    newString = `<a href="mailto:${email}"<${userName}</a>;
+    <span> ${usersMessage} </span>`;
+    
+//remove button
+const removeButton = document.createElement("button");
+removeButton.innerText = "Remove";
+removeButton.setAttribute("button");
+removeButton.addEventListener("click", removeButton);
+
+let entry = document.getElementById('removeButton').parentNode.nodeName;
+
+remove(entry);
+
+// reset form 
+// document.getElementById("leave_message").reset(); 
+document.getElementById("form").reset();
+}
+
+//message form submit
+const messageForm = document.getElementByName("leave_message");
+messageForm[0].addEventListener("submit", onFormSubmit); 
